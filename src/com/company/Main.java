e652e2db3d0430ac08821f3d3a8033f79d0f4aec
Hanif Ali <ali.hanif@outlook.com>
2021-12-01 12:50:37 +0000

    added challenges to main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    int sum = Challenge1();
+
+    System.out.println(sum);
+
+    String[] output_array = sortByLength("how ar you today");
+
+
+    System.out.println(Arrays.asList(output_array));
+    }
+    public static int Challenge1(){
+        int total_sum = 0;
+        for (int i = 0; i< 1000; i++)
+        {
+            if(i % 3 == 0)
+                total_sum = total_sum + i;
+            else if (i % 5 == 0)
+                total_sum = total_sum + i;
+        }
+        return total_sum;
+    }
+
+    public static String[] sortByLength(String input){
+        String[] array = input.split(" ");
+        Arrays.sort(array, (a, b)->Integer.compare(a.length(), b.length()));
+        return array;
     }
 }
